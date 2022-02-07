250d0d595eadbf074b8f3555322da3895fc18af2
Luiz Chiaradia <luizexchiaradia@gmail.com>
2022-02-07 17:27:22 -0300

    update

--- my-app/src/UserScreen.js
+++ my-app/src/UserScreen.js
@@ -1,9 +1,20 @@
 //Hooks
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const UserScreen = () => {
-    const [loading, setLoading] = useState(false);
+    const [pageTitle, setPageTitle] = useState('Teste');
+    const [count, setCount] = useState(0);
+    const [loading, setLoading] = useState(false); // useState retorna um array com 2 posições (valor e função de atualizar o valor). O valor é o estado inicial e a função é para atualizar o valor. 
+    useEffect(() => {
+        setPageTitle(`Você clicou ${count} vezes no botão.`);	
+        document.title = pageTitle;
+        // return () => { // Função que será executada quando o componente for desmontado.};
+    }, [count, pageTitle]); // useEffect é uma função que é executada sempre que o componente é montado componentDidMount() e atualizado componentDidUpdate(). Segundo argumento é um array que restringe a execussão da função ao atualizar o estado. Passando o array vazio, a função será executada somente quando o componente for montado componentDidMount().
     return (
-        <button onClick={() => setLoading(true)}>{!loading ? 'Carregando...' : 'Usuário Logado'}</button>
+        <div>
+            <h2>{pageTitle}</h2>
+            <button onClick={() => setLoading(true)}>{!loading ? 'Carregando...' : 'Usuário Logado'}</button><br />
+            <button onClick={() => setCount(count + 1)}>Atualizar Title</button>
+        </div>
      )
 }
