18b81b3bfae6faaaab490e20fea43980cd682055
Luiz Chiaradia <luizexchiaradia@gmail.com>
2022-02-09 16:35:36 -0300

    update

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -4,7 +4,7 @@ import '../App.css';
 
 export const TodoList = (props) => {
     const defaultListItems = [
-        { name: 'Tarefa 1', isCompleted: false },
+        { name: 'Tarefa 1', isCompleted: true },
         { name: 'Tarefa 2', isCompleted: false },
         { name: 'Tarefa 3', isCompleted: false },
         { name: 'Tarefa 4', isCompleted: false },
@@ -12,15 +12,34 @@ export const TodoList = (props) => {
     const [items, setItems] = useState(defaultListItems);
     const [taskValue, setTaskValue] = useState('');
     const handleTaskSubmit = (event) => {
-        setItems([{name: taskValue, isCompleted: false}, ...items]);
+        if (items.find(i => i.name  === taskValue)) {
+            event.preventDefault();
+            console.log('Tarefa já existe!');
+            return;
+        }
+        setItems([{ name: taskValue, isCompleted: false }, ...items]);
         setTaskValue('');
         event.preventDefault();
     }
 
+    //Ponte entre o componente interno e externo
+    const handleOnTaskChanged = (event, item) => {
+        //Passa por cada item do array e verifica se i.name é igual a item.name recebido e altera o isCompleted
+        setItems(items.map(i => {
+            if (i.name === item.name) {
+                return {
+                    ...i, isCompleted: event.target.checked
+                }
+            }
+            return i;
+        }));
+    }
+
     return (
         <div className='todo-list-container'>
+            <p>Você concluiu {items.filter(i => i.isCompleted).length} tarefas.</p>
             <form onSubmit={handleTaskSubmit}>
-                <input 
+                <input
                     type="text"
                     value={taskValue}
                     placeholder="Adicione uma nova tarefa"
@@ -29,7 +48,9 @@ export const TodoList = (props) => {
                 <button type="submit">Adicionar item</button>
             </form>
             <ul>
-                {items.map((item) => <TodoListItem item={item} />)}
+                {items.map((item) => (
+                    <TodoListItem onTaskChanged={handleOnTaskChanged} item={item} />
+                ))}
             </ul>
         </div>
     )
--- todo-list/src/components/TodoListItem.js
+++ todo-list/src/components/TodoListItem.js
@@ -2,6 +2,8 @@ import '../App.css';
 
 export const TodoListItem = (props) => {
     return (
-        <li><input type="checkbox" /> {props.item.name}</li>
+        <li><input type="checkbox" checked={props.item.isCompleted}
+         onChange={(event) => props.onTaskChanged(event, props.item)}/>
+          {props.item.name}</li>
     )
 };
