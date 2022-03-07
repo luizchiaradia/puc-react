c7374207c46d52c78c3a10f072c7e111bff2e365
luizchiaradia <luizexchiaradia@gmail.com>
2022-03-07 16:48:48 -0300

    update

--- movie-list/src/views/MovieDetail.js
+++ movie-list/src/views/MovieDetail.js
@@ -17,7 +17,17 @@ export const MovieDetail = (props) => {
     }, []);
     return (
         <div className="container">
-            <div>{movie.id}</div>
+            <div className="bg-info p-4 clearfix">
+                <figure className="figure float-start w-25">
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="figure-img img-fluid rounded" alt="..." />
+                </figure>
+                <div className="float-start ps-4 w-75">
+                    <h3>{movie.title}</h3>
+                    <p className="">{movie.overview}</p>
+                    <span className="d-block mb-2">Linguagem Original: {movie.original_language}</span>
+                    <span className="d-block">Nota do Público: {movie.vote_average}</span>
+                </div>
+            </div>
         </div>
     )
 };
